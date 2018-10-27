d736411197a97c60d8ef3340027004ddc13d39a0
Nicolai <nicolailarsen2100@gmail.com>
2018-10-27 21:56:22 +0200

    Hanoi

---
+++ src/Bonus/Hanoi.java
@@ -0,0 +1,18 @@
+package Bonus;
+
+public class Hanoi {
+
+/*    Skriv et program der givet et en integer, n, fra konsollen
+    returnerer trækkene der skal til for at gennemføre et spil towers
+    of hanoi med n skiver. Pindene er angivet som A, B og C og skiverne
+    skal altid flyttes fra A til C.
+    Outputtet skal formatteres som følger ( 3 skiver):
+
+    Disk 1 from A to C
+    Disk 2 from A to B
+    Disk 1 from C to B
+    Disk 3 from A to C
+    Disk 1 from B to A
+    Disk 2 from B to C
+    Disk 1 from A to C*/
+}
