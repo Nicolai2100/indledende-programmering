e61fe70d768880369f68823edfb03221b3cfed37
Nicolai <nicolailarsen2100@gmail.com>
2018-10-27 12:25:14 +0200

    Cinema

--- src/uge7/Cinema.java
+++ src/uge7/Cinema.java
@@ -1,11 +1,19 @@
 package uge7;
 
 public class Cinema {
-    //seatsOnRow;
+
     public static void main(String[] args) {
-        int [] array = {6, 5, 4, 3, 3};
-        Cinema cinema = new Cinema(array);
-        System.out.println(cinema.cinema2D[0][0]);
+        Cinema c = new Cinema(new int[]{6,5,4,3,3,7,10});
+        c.booking(1, 2);
+        c.booking(3, 4);
+        c.release(3, 4);
+        System.out.println("Rows: " +c.rows());
+        System.out.println("Seats on row 5: " +c.seatsOnRow(5));
+        System.out.println("Total number of seats: " + c.seats());
+        System.out.println("Total number of available seats: " +c.vacancies());
+        System.out.println("Avalable seats on row 1: " + c.rowVacancies(1));
+        System.out.println(c);
+
     }
     private int rowsInt;
     private int seatsOnRowInt;
@@ -32,6 +40,7 @@ public class Cinema {
     }
     //giver antal sæder  på række row.
     public int seatsOnRow(int row){
+        if (row <0 || row >= cinema2D.length) return 0;
         seatsOnRowInt = cinema[row];
         return seatsOnRowInt;
     }
@@ -45,6 +54,7 @@ public class Cinema {
 
     //giver antal ledige pladser på række row.
     public int rowVacancies(int row){
+        if (row>= cinema2D.length ||row <0) return 0;
         int count = 0;
         for (int i = 0; i < cinema2D[row].length ; i++) {
             if (cinema2D[row][i]){
@@ -53,31 +63,46 @@ public class Cinema {
         }
         return count;
     }
-
     //giver det samlede antal ledige pladser i biografen.
     public int vacancies() {
         int count = 0;
         for (int i = 0; i < cinema2D.length; i++) {
-            rowVacancies(i);
+            count += rowVacancies(i);
         }
         return count;
     }
     //booker sæde seat på række row.
     public void booking(int row, int seat){
-
-
+        if (checkSeat(row, seat)) return;
+        cinema2D[row][seat] = false;
     }
+
+    private boolean checkSeat(int row, int seat) {
+        if (row >= cinema2D.length || row <0 ||seat >= cinema2D[row].length || seat < 0) return true;
+        return false;
+    }
+
     //ophæver bookingen af sæde seat på række row.
     public void release(int row, int seat) {
-
+        if (checkSeat(row,seat)) return;
+        cinema2D[row][seat] = true;
     }
     //giver en streng som viser hvilke sæder der optaget (#) og hvilke der er
     //    ledige (O).
     public String toString() {
+        String række = "";
+        for (int curRow = 0; curRow < cinema2D.length; curRow++) {
 
-        return ;
+            for (int curSeat = 0; curSeat < cinema2D[curRow].length; curSeat++){
+                if (cinema2D[curRow][curSeat]){
+                    række += "0";
+                }
+                else række += "#";
+            }
+            række+= "\r\n";
+        }
+        return række;
     }
-
 }
 /*
 Som laver et nyt biograf objekt. Antallet af rækker er lig antallet af
