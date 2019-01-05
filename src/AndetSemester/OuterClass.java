734646e3ab10e394fb6945b4a6a0bdf5f4138a8c
Nicolai Larsen <nicolailarsen2100@gmail.com>
2019-01-05 19:56:47 +0100

    ano class2

--- src/AndetSemester/OuterClass.java
+++ src/AndetSemester/OuterClass.java
@@ -1,33 +1,47 @@
 package AndetSemester;
 
+interface ABC{
+
+    void show();
+}
 public class OuterClass {
     int a = 5;
 
-    void showA() {
+    void show() {
         System.out.println(a);
     }
 }
     class InnerClass extends OuterClass{
 
-        void showA(){
+        void show(){
             System.out.println("Sniksnak");
         }
 
     public static void main(String[] args) {
         //Outer class
         OuterClass A = new OuterClass();
-        A.showA();
+        A.show();
 
         //Inner class
         OuterClass B = new InnerClass();
-        B.showA();
+        B.show();
 
         //Inner anonymous class
         OuterClass A2 = new OuterClass(){
-            void showA(){
+            void show(){
                 System.out.println("I am the best!");
             }
         };
-        A2.showA();
+        A2.show();
+
+        //Objekt af interface implementeret som anonym klasse!
+        //Koden kan ikke genbruges
+        ABC obj = new ABC(){
+            @Override
+            public void show() {
+                System.out.println("For vildt!");
+            }
+        };
+        obj.show();
     }
 }
